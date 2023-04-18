a1ae4f1231b6734e12133be7b5a20e1fd1ba4ea8
Özgür Gülsuna <ozgurgulsuna@gmail.com>
2023-04-19 02:14:47 +0300

    [HW1] hamsterNET basic ANN

--- Homeworks/HW1/HamsNET.py
+++ Homeworks/HW1/HamsNET.py
@@ -27,6 +27,17 @@ import numpy as np
 
 import time
 
+batch_size = 40
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# functions to show an image
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -37,14 +48,69 @@ transform = transforms.Compose([
             torchvision.transforms.Grayscale()
             ])
 
+# test set
+test_data = torchvision.datasets.CIFAR10('./data', train = False,transform = transform)
+print('test_data: ', len(test_data))
+
 # training set
 train_data = torchvision.datasets.CIFAR10('./data', train = True, download = True,transform = transform)
 
-# test set
-test_data = torchvision.datasets.CIFAR10('./data', train = False,transform = transform)
+# split the training set into training and validation set
+random_split = torch.utils.data.random_split(train_data, [int(len(train_data)*90/100), int(len(train_data)*10/100)])
+train_data = random_split[0]
+val_data = random_split[1]
+print('train_data: ', len(train_data))
+print('val_data: ', len(val_data))
 
-train_generator = torch.utils.data.DataLoader(train_data, batch_size = 96, shuffle = True)
-test_generator = torch.utils.data.DataLoader(test_data, batch_size = 96, shuffle = False)
+
+# +-----------------------------------------------------------------------------------+
+# |                                       Dataset                                     |
+# | █████████████████████████████████████████████████████████████████████████████████ |
+# |                                        60K                                        |
+# +-----------------------------------------------------------------------------------+
+# |                           Training                           | Val. |    Test     |
+# | =============================================================|======|============ |
+# | █████████████████████████████████████████████████████████████|██████|████████████ |
+# |                              45K                             |  5K  |     10K     |
+# +-----------------------------------------------------------------------------------+
+
+
+
+# data loader
+train_generator = torch.utils.data.DataLoader(train_data, batch_size = batch_size, shuffle = True)
+test_generator = torch.utils.data.DataLoader(test_data, batch_size = batch_size, shuffle = False)
+
+
+
+# scale the data input to [-1,1]
+#train_data.data = (train_data.data - 127.5) / 127.5
+#val_data.data = (val_data.data - 127.5) / 127.5
+# test_data.data = (test_data.data - 127.5) / 127.5
+
+# visualize the data
+# image = train_data[0]
+# image = image.reshape(3,32,32)
+# plt.imshow(image)
+
+dataiter = iter(train_generator)
+images, labels = next(dataiter)
+imshow(torchvision.utils.make_grid(images))
+print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class FullyConnected(torch.nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
@@ -54,12 +120,12 @@ class FullyConnected(torch.nn.Module):
         self.fc2 = torch.nn.Linear(hidden_size, num_classes)
         self.relu = torch.nn.ReLU()
 
-def forward(self, x):
-    x = x.view(-1, self.input_size)
-    hidden = self.fc1(x)
-    relu = self.relu(hidden)
-    output = self.fc2(relu)
-    return output
+    def forward(self, x):
+        x = x.view(-1, self.input_size)
+        hidden = self.fc1(x)
+        relu = self.relu(hidden)
+        output = self.fc2(relu)
+        return output
 
 # initialize your model
 model_mlp = FullyConnected(1024,128,10)
@@ -71,7 +137,9 @@ params_784x128 = model_mlp.fc1.weight.data.numpy()
 loss = torch.nn.CrossEntropyLoss()
 
 # create optimizer
-optimizer = torch.optim.SGD(model_mlp.parameters(), lr = 0.01, momentum = 0.0)
+# optimizer = torch.optim.SGD(model_mlp.parameters(), lr = 0.01, momentum = 0.0)
+optimizer = torch.optim.Adam(model_mlp.parameters(), lr = 0.001)
+
 
 # transfer your model to train mode
 model_mlp.train()
