be76ed90bbfabf708bb4f7e0223b2db356aa251d
Özgür Gülsuna <ozgurgulsuna@gmail.com>
2023-04-20 23:21:05 +0300

    [HW1] "mlp_1" is trained w/ batch: 50 epoch: 15

--- Homeworks/HW1/HamsNET.py
+++ Homeworks/HW1/HamsNET.py
@@ -19,34 +19,27 @@
 # PyTorch MLP tutorial: https://colab.research.google.com/github/bentrevett/pytorch-image-classification/blob/master/1_mlp.ipynb#scrollTo=FTvjOcbLREwM
 
 
+# Imports --------------------------------------------------------------------------------------------------------------------------------------------#
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 import torchvision
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
+
 import numpy as np
+import matplotlib.pyplot as plt
+from tqdm.notebook import trange, tqdm
 
 import time
 
-batch_size = 2
+# Parameters ------------------------------------------------------------------------------------------------------------------------------------#
+validation_ratio = 0.1
+batch_size = 50
 epoch_size = 15
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-
-
-
-# functions to show an image
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-
-
-
-# Hyper-parameters
+# Transformations ------------------------------------------------------------------------------------------------------------------------------------#
 transform = transforms.Compose([
             torchvision.transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
@@ -54,14 +47,15 @@ transform = transforms.Compose([
             torchvision.transforms.Grayscale()
             ])
 
+# Data ----------------------------------------------------------------------------------------------------------------------------------------------#
 # test set
-test_data = torchvision.datasets.CIFAR10('./data', train = False,transform = transform)
+test_data = torchvision.datasets.CIFAR10('./data', train = False, download = True, transform = transform)
 
 # training set
 train_data_original = torchvision.datasets.CIFAR10('./data', train = True, download = True,transform = transform)
 
 # split the training set into training and validation set
-train_data, val_data = torch.utils.data.random_split(train_data_original, [int(len(train_data_original)*90/100), int(len(train_data_original)*10/100)])
+train_data, val_data = torch.utils.data.random_split(train_data_original, [int(len(train_data_original)*(1-validation_ratio)), int(len(train_data_original)*validation_ratio)])
 
 # lenghts of each set
 print('train_data: ', len(train_data))
@@ -80,12 +74,13 @@ print('test_data: ', len(test_data))
 # +-----------------------------------------------------------------------------------+
 
 
-# data loader
+# Data loader ----------------------------------------------------------------------------------------------------------------------------------------#
 train_generator = torch.utils.data.DataLoader(train_data, batch_size = batch_size, shuffle = True)
-val_generator = torch.utils.data.DataLoader(val_data, batch_size = batch_size, shuffle = True)
-test_generator = torch.utils.data.DataLoader(test_data, batch_size = batch_size, shuffle = True)
+val_generator = torch.utils.data.DataLoader(val_data, batch_size = batch_size)
+test_generator = torch.utils.data.DataLoader(test_data, batch_size = batch_size)
 
-
+# Architectures ---------------------------------------------------------------------------------------------------------------------------------------#
+# "mlp_1" is a simple multi-layer perceptron
 class mlp_1(nn.Module):
     def __init__(self, input_size, output_size):
         super(mlp_1,self).__init__()
@@ -101,76 +96,149 @@ class mlp_1(nn.Module):
         x = self.prediction_layer(x)
         return x
     
+# "mlp_2" is a simple multi-layer perceptron
 
-      
+
+
+    
+
+# Training --------------------------------------------------------------------------------------------------------------------------------------------#
 
 # initialize your model
-model_mlp = mlp_1(input_size=32*32, output_size=10)
+model = mlp_1(input_size=32*32, output_size=10)
 
 # get the parameters 1024x32 layer as numpy array
 # we used sequential model, so we can access the layers by index: model_mlp.fc[0].weight.data.numpy()
-params_1024x32 = model_mlp.fc[0].weight.data.numpy()
+params_1024x32 = model.fc[0].weight.data.numpy()
 
 # create loss: use cross entropy loss)
 criterion = torch.nn.CrossEntropyLoss()
 
 # create optimizer
 # optimizer = torch.optim.SGD(model_mlp.parameters(), lr = 0.01, momentum = 0.0)
-optimizer = torch.optim.Adam(model_mlp.parameters(), lr = 0.001)
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
 
 # Device configuration
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# model = model_mlp.to(device)
-# criterion = criterion.to(device)
+model = model.to(device)
+criterion = criterion.to(device)
 
+def calculate_accuracy(y_pred, y):
+    top_pred = y_pred.argmax(1, keepdim=True)
+    correct = top_pred.eq(y.view_as(top_pred)).sum()
+    accuracy = correct.float() / y.shape[0]
+    return accuracy
 
-for epoch in range(epoch_size):  # loop over the dataset multiple times
-    running_loss = 0.0
-    training_accuracy = 0.0
-    validation_accuracy = 0.0
-    for i, data in enumerate(train_generator, 0):
-        # get the inputs
-        inputs, labels = data
-
-        # zero the parameter gradients
+def train(model, iterator, optimizer, criterion, device):
+    epoch_loss = 0
+    epoch_acc = 0
+    model.train()
+    for (x, y) in tqdm(iterator, disable=True):
+        x = x.to(device)
+        y = y.to(device)
         optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = model_mlp(inputs)
-        loss_size = criterion(outputs, labels)
-        loss_size.backward()
+        y_pred = model(x)
+        loss = criterion(y_pred, y)
+        acc = calculate_accuracy(y_pred, y)
+        loss.backward()
         optimizer.step()
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+def evaluate(model, iterator, criterion, device):
+    epoch_loss = 0
+    epoch_acc = 0
+    model.eval()
+    with torch.no_grad():
+        for (x, y) in tqdm(iterator, disable=True):
+            x = x.to(device)
+            y = y.to(device)
+            y_pred = model(x)
+            loss = criterion(y_pred, y)
+            acc = calculate_accuracy(y_pred, y)
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+# Training loop ---------------------------------------------------------------------------------------------------------------------------------------#
+
+best_valid_loss = float('inf')
+
+for epoch in trange(epoch_size,disable=True):
+
+    start_time = time.monotonic()
+
+    train_loss, train_acc = train(model, train_generator, optimizer, criterion, device)
+    valid_loss, valid_acc = evaluate(model, val_generator, criterion, device)
+
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'mlp_1-model.pt')
+
+    end_time = time.monotonic()
+
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+
+    print(f'Epoch:         {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+    print(f'Train Loss: {train_loss:.3f} |  Train Acc: {train_acc*100:.2f}%')
+    print(f'Val. Loss:  {valid_loss:.3f} |   Val. Acc: {valid_acc*100:.2f}%')
+    print(f'+-----------------------------------------------------------------------------+')
+
+
+# for epoch in range(epoch_size):  # loop over the dataset multiple times
+#     running_loss = 0.0
+#     training_accuracy = 0.0
+#     validation_accuracy = 0.0
+#     for i, data in enumerate(train_generator, 0):
+#         # get the inputs
+#         inputs, labels = data
+
+#         # zero the parameter gradients
+#         optimizer.zero_grad()
+
+#         # forward + backward + optimize
+#         outputs = model_mlp(inputs)
+#         loss_size = criterion(outputs, labels)
+#         loss_size.backward()
+#         optimizer.step()
         
-        # print statistics
-        training_accuracy += (outputs.argmax(1) == labels).sum().item()
-        validation_accuracy += (outputs.argmax(1) == labels).sum().item()
+#         # print statistics
+#         training_accuracy += (outputs.argmax(1) == labels).sum().item()
 
-        running_loss += loss_size.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-            print('Training accuracy: ', training_accuracy / (batch_size * 2000))
-            print('Validation accuracy: ', validation_accuracy / (batch_size * 2000))
-            training_accuracy = 0.0
-            validation_accuracy = 0.0
+#         running_loss += loss_size.item()
+#         if i % 2000 == 1999:    # print every 2000 mini-batches
+#             print('[%d, %5d] loss: %.3f' %
+#                   (epoch + 1, i + 1, running_loss / 2000))
+#             running_loss = 0.0
+#             print('Training accuracy: ', training_accuracy / (batch_size * 2000))
+#             print('Validation accuracy: ', validation_accuracy / (batch_size * 2000))
+#             training_accuracy = 0.0
 
-print('Finished Training')
+# print('Finished Training')
 
 # save your model
 PATH = './cifar_net.pth'
-torch.save(model_mlp.state_dict(), PATH)
+torch.save(model.state_dict(), PATH)
 
 
 
 
 
-# transfer your model to train mode
-model_mlp.train()
+# # transfer your model to train mode
+# model_mlp.train()
 
-# transfer your model to eval mode
-model_mlp.eval()
+# # transfer your model to eval mode
+# model_mlp.eval()
 
 
 
